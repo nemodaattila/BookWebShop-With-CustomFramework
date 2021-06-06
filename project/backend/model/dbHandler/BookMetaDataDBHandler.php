ea8ae7658f15c02fc80bed0b16e0cafd72817cc3
nemodaattila <nemodaattila@gmail.com>
2021-06-06 17:16:11 +0200

    Add files via upload

--- project/backend/model/dbHandler/BookMetaDataDBHandler.php
+++ project/backend/model/dbHandler/BookMetaDataDBHandler.php
@@ -1,122 +1,157 @@
 <?php
 
-
 namespace project\backend\model\dbHandler;
 
-
 use core\backend\database\PDOProcessorBuilder;
-use core\backend\helper\VariableHelper;
+use core\backend\model\RequestResultException;
+use JetBrains\PhpStorm\ArrayShape;
 
+/**
+ * Class BookMetaDataDBHandler könyvekkel kapcsolatos meta-adatok visszaadása adatbázisból
+ * @package project\backend\model\dbHandler
+ */
 class BookMetaDataDBHandler
 {
-    public function getAllMetaData()
+    /**
+     * összes metaadat lekérése, további függvényeken keresztül
+     * @return array a metaadatok asszociativ tömbje
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    #[ArrayShape(['format' => "array", 'language' => "array", 'mainCategory' => "array", 'subCategory' => "array", 'tag' => "array", 'targetAudience' => "array", 'type' => "array"])]
+    public function getAllMetaData(): array
     {
         return [
-            'format'=>$this->getFormat(),
-            'language'=>$this->getLanguage(),
-            'mainCategory'=>$this->getMainCategory(),
-            'subCategory'=>$this->getSubCategory(),
-            'tag'=>$this->getTag(),
-            'targetAudience'=>$this->getTargetAudience(),
-            'type'=>$this->getType(),
+            'format' => $this->getFormat(),
+            'language' => $this->getLanguage(),
+            'mainCategory' => $this->getMainCategory(),
+            'subCategory' => $this->getSubCategory(),
+            'tag' => $this->getTag(),
+            'targetAudience' => $this->getTargetAudience(),
+            'type' => $this->getType(),
         ];
     }
 
-    private function getFormat()
+    /**
+     * formátumtipusok visszadása
+     * @return array multidimenzionális tömb pl: ['könyv'=>[0=>'puhafedeles', ...], ['hangoskönyv'=>[10=>'audiocd']]]
+     * @throws RequestResultException PDOProcesszot hiba
+     */
+    private function getFormat(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
         $PDOLink->setCommand("SELECT id, type_id, name FROM meta_format");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            if (!isset($result[$value['type_id']]))
-            {
-                $result[$value['type_id']]=[];
+        foreach ($tempResult as $value) {
+            if (!isset($result[$value['type_id']])) {
+                $result[$value['type_id']] = [];
             }
-            $result[$value['type_id']][$value['id']]=$value['name'];
+            $result[$value['type_id']][$value['id']] = $value['name'];
         }
         return $result;
     }
 
-    private function getLanguage()
+    /**
+     * létező nyelvek visszaadása
+     * @return array a nyelvek tömbje
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    private function getLanguage(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
         $PDOLink->setCommand("SELECT id,  name FROM meta_language");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            $result[$value['id']]=$value['name'];
+        foreach ($tempResult as $value) {
+            $result[$value['id']] = $value['name'];
         }
         return $result;
     }
 
-    private function getMainCategory()
+    /**
+     * fő téma kategóriák visszadása (pl: szép és szórakoztató irodalom)
+     * @return array kategóriák tömbje
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    private function getMainCategory(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
-            $PDOLink->setCommand("SELECT id,  name FROM meta_main_category");
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink->setCommand("SELECT id,  name FROM meta_main_category");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            $result[$value['id']]=$value['name'];
+        foreach ($tempResult as $value) {
+            $result[$value['id']] = $value['name'];
         }
         return $result;
     }
 
-    private function getSubCategory()
+    /**
+     * al-téma kategóriák visszadása (pl: [szép és szórakoztató irodalom=>[szépirodalom, képregény]])
+     * @return array kategóriák multidimenziónális tömbje
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    private function getSubCategory(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
         $PDOLink->setCommand("SELECT id, main_category_id, name FROM meta_subcategory");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            if (!isset($result[$value['main_category_id']]))
-            {
-                $result[$value['main_category_id']]=[];
+        foreach ($tempResult as $value) {
+            if (!isset($result[$value['main_category_id']])) {
+                $result[$value['main_category_id']] = [];
             }
-            $result[$value['main_category_id']][$value['id']]=$value['name'];
+            $result[$value['main_category_id']][$value['id']] = $value['name'];
         }
         return $result;
     }
 
-    private function getTag()
+    /**
+     * könyv tegek visszaadása [pl: scifi, háborús, romantika]
+     * @return array tagek tömbje
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    private function getTag(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
         $PDOLink->setCommand("SELECT id,  name FROM meta_tag");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            $result[$value['id']]=$value['name'];
+        foreach ($tempResult as $value) {
+            $result[$value['id']] = $value['name'];
         }
         return $result;
     }
 
-    private function getTargetAudience()
+    /**
+     * létező célcsoportok visszadása pl: all age, 18+
+     * @return array célcsoportok tömbje
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    private function getTargetAudience(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
         $PDOLink->setCommand("SELECT id,  name FROM meta_target_audience");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            $result[$value['id']]=$value['name'];
+        foreach ($tempResult as $value) {
+            $result[$value['id']] = $value['name'];
         }
         return $result;
     }
 
-    private function getType()
+    /**
+     * könyvtipusok visszaadása
+     * @return array ['könyv', 'e-book','hangoskönyv']
+     * @throws RequestResultException PDOProcesszor hiba
+     */
+    private function getType(): array
     {
-        $PDOLink=PDOProcessorBuilder::getProcessor('select', true);
+        $PDOLink = PDOProcessorBuilder::getProcessor('select', true);
         $PDOLink->setCommand("SELECT id,  name FROM meta_type");
         $tempResult = $PDOLink->execute();
         $result = [];
-        foreach ($tempResult as $value)
-        {
-            $result[$value['id']]=$value['name'];
+        foreach ($tempResult as $value) {
+            $result[$value['id']] = $value['name'];
         }
         return $result;
     }
